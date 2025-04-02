a937de34dfe1bee84b2faf543111bc962ebff690
Tinchodomi <tinchodomi@gmail.com>
2025-04-02 19:27:02 -0300

    Agrego TP1 Secuenciales

---
+++ 01 Estructuras Secuenciales/Dominguez_Martin_TP1.py
@@ -0,0 +1,92 @@
+import math
+
+#ejercicio1
+'''
+print("Hola Mundo!")
+
+#ejercicio2
+
+nombre = input("Ingrese su nombre:")
+print(f"Hola {nombre}!")
+
+
+#ejercicio 3
+
+nombre = input("Ingrese su nombre:")
+apellido = input("Ingrese su apellido:")
+edad= input("Ingrese su edad:")
+residencia = input("Ingrese su residencia:")
+print(f"Soy {nombre} {apellido}, tengo {edad} años y vivo en {residencia}")
+
+
+#ejercicio 4
+radio = int(input("Ingrese el Radio del Circulo para calcular su Area y Perímetro:"))
+perimetro = radio**2 * math.pi
+area = radio * math.pi
+print(f"El area es {area} y el perimetro es {perimetro}")'
+
+
+#ejercicio 5
+
+segundos = int(input("Calculadora segundos a horas:"))
+horas = segundos / 3600
+print(f"Horas: {horas}")
+
+
+ #ejercicio 6
+
+
+
+numero = int (input ("Ingrese un numero:"))
+
+for i in range (1,11):
+    resultado = numero * i
+    print(f"{numero} x {i} = {resultado}")
+
+
+#ejercicio 7
+
+numero1 = int (input ("Ingrese un numero:"))
+numero2 = int (input ("Ingrese otro numero:"))
+
+suma = numero1+numero2
+resta = numero1-numero2
+multiplicacion = numero1*numero2
+division = numero1/numero2
+
+print(f"Suma: {suma}\nResta {resta}\nMultiplicacion: {multiplicacion}\nDivision: {division}")
+
+#ejercicio 8
+
+altura = int(input("Ingrese su altura en centimetros:"))
+peso = int(input("Ingrese su peso en kilogramos:"))
+
+imc = peso / altura**2
+
+print("El Indice de masa corporal es: " , imc)'
+
+#ejercicio9
+
+celsius = int(input("Ingrese los Grados Celsius: "))
+
+fahrenheit = celsius * (9/5) + 32
+
+print ("Los Fahrenheit son: ", fahrenheit)
+
+
+
+
+numero1 = int (input ("Ingrese primer numero:"))
+numero2 = int (input ("Ingrese segundo numero:"))
+numero3 = int (input ("Ingrese tercer numero:"))
+
+promedio = (numero1 + numero2 + numero3) / 3
+
+print("El promedio es:", promedio)
+
+'''
+a = 10
+b = 3.0
+c = a * b
+d = a + b
+print(f'c = {c}, d = {d}')
